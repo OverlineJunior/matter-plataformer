335eb2576716816862621ef51648343e8d8b7f1b
OverlineJunior <franciscosebastianyjunior@hotmail.com>
2022-07-02 18:23:28 -0300

    fix character trying to face both foward and backward when both A and D are down.

--- src/Client/Systems/CharacterFacesMovement.lua
+++ src/Client/Systems/CharacterFacesMovement.lua
@@ -1,7 +1,6 @@
 local UserInputService = game:GetService('UserInputService')
 local ReplicatedStorage = game:GetService('ReplicatedStorage')
 
-local Matter = require(ReplicatedStorage.Packages.Matter)
 local Components = require(ReplicatedStorage.Client.Components)
 
 local Player = Components.Player
@@ -17,9 +16,16 @@ local function CharacterFacesMovement(world, _, widgets)
 
         char.Humanoid.AutoRotate = widgets.checkbox('Enable AutoRotate'):checked()
 
-        if UserInputService:IsKeyDown(Enum.KeyCode.A) and root.Orientation.Y ~= 90 then
+        if UserInputService:IsKeyDown(Enum.KeyCode.A)
+            and not UserInputService:IsKeyDown(Enum.KeyCode.D)
+            and root.Orientation.Y ~= 90
+        then
             root.CFrame = CFrame.new(root.Position) * CFrame.Angles(0, math.rad(90), 0)
-        elseif UserInputService:IsKeyDown(Enum.KeyCode.D) and root.Orientation.Y ~= -90 then
+
+        elseif UserInputService:IsKeyDown(Enum.KeyCode.D)
+            and not UserInputService:IsKeyDown(Enum.KeyCode.A)
+            and root.Orientation.Y ~= -90
+        then
             root.CFrame = CFrame.new(root.Position) * CFrame.Angles(0, math.rad(-90), 0)
         end
     end
